741c4592a5e64a7e51a05cb113245c566c45b333
ivenlaw <78380234+ivenlaw@users.noreply.github.com>
2023-08-15 22:52:15 +0000

    15/08/2023

--- .ccls-cache/@home@runner@INF029-GeorgianaLaw/aulas@primeiro.c
+++ .ccls-cache/@home@runner@INF029-GeorgianaLaw/aulas@primeiro.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
-// #define TAM 3
+#define TAM 10
 int main(void) {
   
-  int idades[10], media, soma=0, i;
+  int idades[TAM], media, soma=0, i;
 
-  for(i=0; i<10; i++) {
-    printf("Digite 10 idades:\n");
+  for(i=0; i<TAM; i++) {
+    printf("Digite %d idades:\n", TAM);
     scanf("%d", &idades[i]);
     soma=soma+idades[i];
   }
 
-  media = soma/10;
+  media = soma/TAM;
 
-  for(i=0; i<10; i++) {
+  for(i=0; i<TAM; i++) {
     if (idades[i]>media)
-      printf("%d", idades[i]);
+      printf("%d\n", idades[i]);
   }
 }
--- Projeto Escola/Alunos.c
+++ Projeto Escola/Alunos.c
@@ -1,21 +1,23 @@
 /* Cadastro de Alunos */
 
 #include <stdio.h>
+#include <string.h>
 #define tamAluno 3
 
 typedef struct {
 
   int matriculaAluno;
-  char nomeAluno;
+  char nomeAluno[51];
   char sexoAluno;
   int nascimentoAluno;
-  int cpfAluno;
+  char cpfAluno[12];
 } Aluno;
 
-int main {
+int main () {
 
   
-
+  
+return 0;
 }
 
 
